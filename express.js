7bb811918e047a589668ef9674796ceacd14099e
Thomas Yoho <TYohoJr@Gmail.com>
2018-04-02 16:25:42 -0600

    added server text logger

--- express.js
+++ express.js
@@ -2,17 +2,26 @@ var express = require("express");
 var app = express();
 var path = require('path')
 require('dotenv').config();
+var twilio = require('twilio');
 
 app.use(express.static(path.join(__dirname, "build")));
 
+var client = new twilio(`${process.env.TW_SID}`, `${process.env.TW_TOKEN}`);
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
 
 app.listen(process.env.PORT || 8080, () => {
     var curPort = process.env.PORT;
     if (curPort === undefined) {
         curPort = "localhost://8080"
+    } else {
+        client.messages.create({
+            to: `+16309622093`,
+            from: '+12314653515',
+            body: `Profile view on ${new Date()}`
+        });
+        console.log(`listening on ${curPort}`)
     }
-    console.log(`listening on ${curPort}`)
 })
