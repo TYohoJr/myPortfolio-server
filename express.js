63bce6bbdf8ac7f80124dd4e4a7c717c4ec37ed0
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-16 09:28:26 -0500

    cleaned up some old code

--- express.js
+++ express.js
@@ -2,12 +2,10 @@ var express = require("express");
 var app = express();
 var path = require('path')
 require('dotenv').config();
-var twilio = require('twilio');
 
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get('/', function (req, res) {
-    console.log('build file sent')
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
@@ -18,11 +16,6 @@ app.listen(process.env.PORT || 8080, () => {
         curPort = "localhost://8080"
     } else {
         let d = new Date();
-        // client.messages.create({
-        //     to: `+16309622093`,
-        //     from: '+12314653515',
-        //     body: `Profile view on ${d}`
-        // });
         console.log(`listening on ${curPort} at ${d}`)
     }
 })
